9d95e6db04c34c7a9d3ab751abf1d134a95342f3
soyalexokei <alejandrofernandezperez@protonmail.com>
2024-03-16 11:28:25 +0100

    10º commit

--- js/aficion1.js
+++ js/aficion1.js
@@ -2,30 +2,30 @@ const pantallaAficion1 = document.getElementById("imgs-aficion1");
 const flechaIzquierda1 = document.getElementById("flecha-izquierda1");
 const flechaDerecha1 = document.getElementById("flecha-derecha1");
 
-const totalImagenes1 = 49;
-let imagenActual1 = 1;
+const cupoImagenes1 = 49;
+let presente1 = 1;
 
 function cambiarImagen1(cont1) {
-  if (cont1 < 1) {
-    cont1 = totalImagenes1;
-  } else if (cont1 > totalImagenes1) {
-    cont1 = 1;
+  if (presente1 < 1) {
+    presente1 = cupoImagenes1;
+  } else if (presente1 > cupoImagenes1) {
+    presente1 = 1;
   }
-  pantallaAficion1.style.transform = `translateX(-${(cont1 - 1) * 100}%)`;
+  pantallaAficion1.style.transform = `translateX(-${(presente1 - 1) * 100}%)`;
 }
 
 flechaIzquierda1.addEventListener("click", () => {
-  imagenActual1--;
-  cambiarImagen1(imagenActual1);
+  presente1--;
+  cambiarImagen1(presente1);
 });
 
 flechaDerecha1.addEventListener("click", () => {
-  imagenActual1++;
-  cambiarImagen1(imagenActual1);
+  presente1++;
+  cambiarImagen1(presente1);
 });
 
-for (let i = 1; i <= totalImagenes1; i++) {
-  const imagen1 = document.createElement("img");
-  imagen1.src = `./img/Coches/${i}.jpg`;
-  pantallaAficion1.appendChild(imagen1);
+for (let i = 1; i <= cupoImagenes1; i++) {
+  const marco1 = document.createElement("img");
+  marco1.src = `./img/Coches/${i}.jpg`;
+  pantallaAficion1.appendChild(marco1);
 }
--- js/album.js
+++ js/album.js
@@ -2,30 +2,30 @@ const pantallaAlbum = document.getElementById('imgs-album');
 const flechaIzquierda2 = document.getElementById("flecha-izquierda2");
 const flechaDerecha2 = document.getElementById("flecha-derecha2");
 
-const totalImagenes2 = 6;
-let imagenActual2 = 1;
+const cupoImagenes2 = 6;
+let presente2 = 1;
 
 function cambiarImagen2(cont2) {
-  if (cont2 < 1) {
-    cont2 = totalImagenes2;
-  } else if (cont2 > totalImagenes2) {
-    cont2 = 1;
+  if (presente2 < 1) {
+    presente2 = cupoImagenes2;
+  } else if (presente2 > cupoImagenes2) {
+    presente2 = 1;
   }
-  pantallaAlbum.style.transform = `translateX(-${(cont2 - 1) * 100}%)`;
+  pantallaAlbum.style.transform = `translateX(-${(presente2 - 1) * 100}%)`;
 }
 
 flechaIzquierda2.addEventListener("click", () => {
-  imagenActual2--;
-  cambiarImagen2(imagenActual2);
+  presente2--;
+  cambiarImagen2(presente2);
 });
 
 flechaDerecha2.addEventListener("click", () => {
-  imagenActual2++;
-  cambiarImagen2(imagenActual2);
+  presente2++;
+  cambiarImagen2(presente2);
 });
 
-for (let i = 1; i <= totalImagenes2; i++) {
-  const imagen2 = document.createElement("img");
-  imagen2.src = `./img/Portada/${i}.png`;
-  pantallaAlbum.appendChild(imagen2);
+for (let i = 1; i <= cupoImagenes2; i++) {
+  const marco2 = document.createElement("img");
+  marco2.src = `./img/Portada/${i}.png`;
+  pantallaAlbum.appendChild(marco2);
 }
