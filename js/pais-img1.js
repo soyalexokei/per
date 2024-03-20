4f53488b4e84fdad06368276dc525b7cc7745511
soyalexokei <alejandrofernandezperez@protonmail.com>
2024-03-20 23:25:00 +0100

    15º commit

--- js/pais-img1.js
+++ js/pais-img1.js
@@ -2,51 +2,8 @@ const pantallaPais1 = document.getElementById('imgs-pais1');
 const flechaIzquierda3 = document.getElementById("flecha-izquierda3");
 const flechaDerecha3 = document.getElementById("flecha-derecha3");
 
-const totalPais1 = 162;
+const totalPais1 = 152;
 let presente3 = 1;
-var primerIDpais1 = '-X-8jDfjExo';
-var player;
-
-function playerGo1(kP1) {
-  if(player) {
-      player.loadVideoById(kP1);
-  }else {
-      player = new YT.Player('imgs-pais1', {
-          videoId: kP1, // Reemplaza ID_DEL_VIDEO con el ID de tu video de YouTube
-          playerVars: {
-              'autoplay': 0, // Configura si el video se reproduce automáticamente (0 o 1)
-              'controls': 1, // Configura si se muestran los controles del reproductor (0 o 1)
-              'rel': 0, // Configura si se muestran videos relacionados al final (0 o 1)
-              'showinfo': 0 // Configura si se muestra el título del video y la barra de reproducción al inicio (0 o 1)
-          }
-      });
-  }
-}
-
-function obtenerVideoId1(indexP1) {
-  switch(indexP1) {
-      case 153:
-        return '-X-8jDfjExo';
-      case 154:
-        return 'zkhfF_SHNjA';
-      case 155:
-        return 'E2e1P1dsUYg';
-      case 156:
-        return 'Abq0iuBNCBU';
-      case 157:
-        return 'N4dxwAgW_X4';
-      case 158:
-        return 'sr1pmvg9KIs';
-      case 159:
-        return 'j-K_xTuppJQ';
-      case 160:
-        return '50TjMT4zGrc';
-      case 161:
-        return 'DW--iP-CleY';
-      case 162:
-        return 'AY6cu8ShCQA';
-  }
-}
 
 function reproduccionVideos1(contP1) {
   var videoPaisId = obtenerVideoId1(presente3);
---
+++ js/pais-video1.js
@@ -0,0 +1,87 @@
+const pantallaPaisVideo1 = document.getElementById('videos-pais1');
+const flechaIzquierda4 = document.getElementById("flecha-izquierda4");
+const flechaDerecha4 = document.getElementById("flecha-derecha4");
+
+var primerIDpais1 = '6E8OuGFDB8s';
+var player1;
+const totalPaisVideo1 = 17;
+let presenteVideo3 = 1;
+
+function playerGo1(kP1) {
+    if(player1) {
+        player1.loadVideoById(kP1);
+    }else {
+        player1 = new YT.Player(pantallaPaisVideo1, {
+            videoId: kP1, // Reemplaza ID_DEL_VIDEO con el ID de tu video de YouTube
+            playerVars: {
+                'autoplay': 0, // Configura si el video se reproduce automáticamente (0 o 1)
+                'controls': 1, // Configura si se muestran los controles del reproductor (0 o 1)
+                'rel': 0, // Configura si se muestran videos relacionados al final (0 o 1)
+                'showinfo': 0 // Configura si se muestra el título del video y la barra de reproducción al inicio (0 o 1)
+            }
+        });
+    }
+}
+
+function obtenerVideoId1(indexP1) {
+    switch(indexP1) {
+        case 1:
+            return '6E8OuGFDB8s';
+        case 2:
+            return 'jA_PeI3VieM';
+        case 3:
+            return '-X-8jDfjExo';
+        case 4:
+            return 'zkhfF_SHNjA';
+        case 5:
+            return 'E2e1P1dsUYg';
+        case 6:
+            return 'Abq0iuBNCBU';
+        case 7:
+            return 'N4dxwAgW_X4';
+        case 8:
+            return 'sr1pmvg9KIs';
+        case 9:
+            return 'j-K_xTuppJQ';
+        case 10:
+            return '50TjMT4zGrc';
+        case 11:
+            return 'DW--iP-CleY';
+        case 12:
+            return 'AY6cu8ShCQA';
+        case 13:
+            return 'VMuzCFVrCgQ';
+        case 14:
+            return 'MX_iH72HrKE';
+        case 15:
+            return 'r0sSaHApP8w';
+        case 16:
+            return 'kBAjLuGFMw0';
+        case 17:
+            return '2ZReINrYpKQ';
+    }
+}
+
+flechaIzquierda4.addEventListener("click", () => {
+    presente3--;
+    if(presente3 >= 1 && presente3 <= totalPais1-10) {
+      cambiarImagen3(presente3);
+    }else if(presente3 >= 153 && presente3 <= totalPais1) {
+      reproduccionVideos1(presente3);
+    }else if(presente3 < 1) {
+      presente3 = totalPais1;
+      reproduccionVideos1(presente3);
+    }
+});
+
+flechaDerecha4.addEventListener("click", () => {
+  presente3++;
+  if(presente3 >= 1 && presente3 <= totalPais1-10) {
+    cambiarImagen3(presente3);
+  }else if(presente3 >= 153 && presente3 <= totalPais1) {
+    reproduccionVideos1(presente3);
+  }else if(presente3 > totalPais1) {
+    presente3 = 1;
+    cambiarImagen3(presente3);
+  }
+});
