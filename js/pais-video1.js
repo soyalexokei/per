11599a117f51afbe62ffe2908b5c467b901d5e25
soyalexokei <alejandrofernandezperez@protonmail.com>
2024-04-04 22:00:43 +0200

    23º commit

--- js/pais-video1.js
+++ js/pais-video1.js
@@ -1,89 +1,29 @@
-const flechaIzquierda4 = document.getElementById("flecha-izquierda4");
-const flechaDerecha4 = document.getElementById("flecha-derecha4");
+var videosIDusa = ["nf1kb3TSD9w", "6E8OuGFDB8s", "jA_PeI3VieM", "-X-8jDfjExo", "zkhfF_SHNjA", "E2e1P1dsUYg", "Abq0iuBNCBU", "N4dxwAgW_X4", "sr1pmvg9KIs", "j-K_xTuppJQ", "50TjMT4zGrc", "DW--iP-CleY", "AY6cu8ShCQA", "VMuzCFVrCgQ", "MX_iH72HrKE", "r0sSaHApP8w", "kBAjLuGFMw0", "2ZReINrYpKQ"];
+var indiceUSA = 0;
 
-let presenteVideo1 = 1;
-const totalPaisVideo1 = 18;
-var primerVideoPaisId = 'nf1kb3TSD9w';
-var player;
+// Función para agregar videos al carrusel
+function cargarVideos1() {
 
-function playerGo(ident) {
-    if(player) {
-        player.loadVideoById(ident);
-    }else {
-        player = new YT.Player('videos-pais1', {
-            videoId: ident, // Reemplaza ID_DEL_VIDEO con el ID de tu video de YouTube
-            playerVars: {
-                'autoplay': 0, // Configura si el video se reproduce automáticamente (0 o 1)
-                'controls': 1, // Configura si se muestran los controles del reproductor (0 o 1)
-                'rel': 0, // Configura si se muestran videos relacionados al final (0 o 1)
-                'showinfo': 0 // Configura si se muestra el título del video y la barra de reproducción al inicio (0 o 1)
-            }
-        });
-    }
+    var videosUSA = document.getElementById("videos-pais1");
+    videosUSA.innerHTML = "";
+
+    var iframe = document.createElement("iframe");
+    iframe.src = "https://www.youtube.com/embed/" + videosIDusa[indiceUSA];
+    iframe.allowFullscreen = true;
+    videosUSA.appendChild(iframe);
 }
 
-function obtenerVideoId1(indexP1) {
-    switch(indexP1) {
-        case 1:
-            return 'nf1kb3TSD9w';
-        case 2:
-            return '6E8OuGFDB8s';
-        case 3:
-            return 'jA_PeI3VieM';
-        case 4:
-            return '-X-8jDfjExo';
-        case 5:
-            return 'zkhfF_SHNjA';
-        case 6:
-            return 'E2e1P1dsUYg';
-        case 7:
-            return 'Abq0iuBNCBU';
-        case 8:
-            return 'N4dxwAgW_X4';
-        case 9:
-            return 'sr1pmvg9KIs';
-        case 10:
-            return 'j-K_xTuppJQ';
-        case 11:
-            return '50TjMT4zGrc';
-        case 12:
-            return 'DW--iP-CleY';
-        case 13:
-            return 'AY6cu8ShCQA';
-        case 14:
-            return 'VMuzCFVrCgQ';
-        case 15:
-            return 'MX_iH72HrKE';
-        case 16:
-            return 'r0sSaHApP8w';
-        case 17:
-            return 'kBAjLuGFMw0';
-        case 18:
-            return '2ZReINrYpKQ';
-    }
-}
-
-function reproductorVideoPais1(contP1) {
-
-    if(contP1 < 1) {
-        contP1 = totalPaisVideo1;
-    }else if(contP1 > totalPaisVideo1) {
-        contP1 = 1;
-    }
-
-    var videoPaisId = obtenerVideoId1(contP1);
-    playerGo(videoPaisId);
-    presenteVideo1 = contP1;
-}
-
-flechaIzquierda4.addEventListener("click", () => {
-    presenteVideo1--;
-    reproductorVideoPais1(presenteVideo1);
+document.getElementById("flecha-izquierda4").addEventListener("click", () => {
+    indiceUSA = (indiceUSA - 1 + videosIDusa.length) % videosIDusa.length;
+    cargarVideos1();
 });
 
-flechaDerecha4.addEventListener("click", () => {
-    presenteVideo1++;
-    reproductorVideoPais1(presenteVideo1);
+document.getElementById("flecha-derecha4").addEventListener("click", () => {
+    indiceUSA = (indiceUSA + 1) % videosIDusa.length;
+    cargarVideos1();
 });
 
-playerGo(primerVideoPaisId);
+// Llamar a la función para cargar los videos cuando se muestre el modal
+$('#pais-video1').on('shown.bs.modal', function () {
+    cargarVideos1();
+});
--- js/pais-video2.js
+++ js/pais-video2.js
@@ -2,7 +2,7 @@ var videosIDportugal = ["s1YN5d8BUlo", "eiqsB64eVO4", "tq02Cn10p9E", "3gLSZA5vM1
 var indicePortugal = 0;
 
 // Función para agregar videos al carrusel
-function cargarVideos() {
+function cargarVideos2() {
 
     var videosPortugal = document.getElementById("videos-pais2");
     videosPortugal.innerHTML = "";
@@ -15,15 +15,15 @@ function cargarVideos() {
 
 document.getElementById("flecha-izquierda6").addEventListener("click", () => {
     indicePortugal = (indicePortugal - 1 + videosIDportugal.length) % videosIDportugal.length;
-    cargarVideos();
+    cargarVideos2();
 });
 
 document.getElementById("flecha-derecha6").addEventListener("click", () => {
     indicePortugal = (indicePortugal + 1) % videosIDportugal.length;
-    cargarVideos();
+    cargarVideos2();
 });
 
 // Llamar a la función para cargar los videos cuando se muestre el modal
 $('#pais-video2').on('shown.bs.modal', function () {
-    cargarVideos();
+    cargarVideos2();
 });
