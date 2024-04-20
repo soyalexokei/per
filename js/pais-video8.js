3893d0b2642459f0dbfe3e8e701c4470baecd556
soyalexokei <alejandrofernandezperez@protonmail.com>
2024-04-20 19:22:02 +0200

    43º commit

--- js/pais-video8.js
+++ js/pais-video8.js
@@ -1,4 +1,8 @@
-var videosIDmontenegro = ["iSQOpOChzYs", "kVJi0DUagIM", "EoioMgzx-ug", "Hllh6hyxrs4", "XVOFNUxsoLg", "U9Bx2euDKtA", "H1ATXgPLjYY", "KZkp8sBK1F0", "Hv_XcfjiPnw", "C2GpNDUg7OY"];
+var videosIDmontenegro = ["qKxMs3R-4UA", "iSQOpOChzYs", "kVJi0DUagIM", "EoioMgzx-ug", "Hllh6hyxrs4",
+"XVOFNUxsoLg", "U9Bx2euDKtA", "H1ATXgPLjYY", "KZkp8sBK1F0", "Hv_XcfjiPnw",
+"C2GpNDUg7OY", "IjIZrfX0Pug", "P2h7P7YN8K8", "8B11Kq0iuzg", "nHdq7DIXZxk",
+"l8m91nOZd6A", "LtLtIsLoV-U", "YJMyqmoDOvE", "6FGl60n7gZg", "lcO9LhNS1u4",
+"EekbRChkwpY"];
 var indiceMontenegro = 0;
 
 // Función para agregar videos al carrusel
