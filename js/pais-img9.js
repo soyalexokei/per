1ee22b9cad535822d81396baad235ba74d8edc5e
soyalexokei <alejandrofernandezperez@protonmail.com>
2024-04-21 21:53:53 +0200

    44º commit

---
+++ js/pais-img9.js
@@ -0,0 +1,30 @@
+const pantallaPais9 = document.getElementById('imgs-pais9');
+
+const totalPais9 = 134;
+let presente11 = 1;
+
+function cambiarImagen11(cont11) {
+  if(presente11 < 1) {
+    presente11 = totalPais9;
+  }else if(presente11 > totalPais9) {
+    presente11 = 1;
+  }
+
+  pantallaPais9.style.transform = `translateX(-${(presente11 - 1) * 100}%)`;
+}
+
+document.getElementById("flecha-izquierda19").addEventListener("click", () => {
+    presente11--;
+    cambiarImagen11(presente11);
+});
+
+document.getElementById("flecha-derecha19").addEventListener("click", () => {
+    presente11++;
+    cambiarImagen11(presente11);
+});
+
+for (let i = 1; i <= totalPais9; i++) {
+  const marco11 = document.createElement("img");
+  marco11.src = `./img/Serbia/${i}.jpg`;
+  pantallaPais9.appendChild(marco11);
+}
