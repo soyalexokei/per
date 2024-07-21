55966c6734ef5cdf3d5ebb669aaafdbad23eb683
soyalexokei <alejandrofernandezperez@protonmail.com>
2024-07-21 21:38:37 +0200

    96º commit

---
+++ js/sobre-mi.js
@@ -0,0 +1,21 @@
+const marcoSobreMi = document.getElementById('marco-descrip-sobre-mi');
+
+const descripcionSobreMi =
+`
+    <p>Soy una persona proactiva que le gusta alcanzar metas, emprender</p>
+    <p>y superar cada bache de la vida. Nací el 30 de marzo de 1997, en</p>
+    <p>Madrid (España)</p><br><br>
+    
+    <p>Me encanta viajar, descubrir nuevas culturas, conocer nuevas personas y</p>
+    <p>tener diversas experiencias a lo largo del mundo. Además de hacer</p>
+    <p>deporte, socializar con amigos y tener una buena alimentación</p>
+    <p>y equilibrada</p><br><br>
+    
+    <p><q><i>La vida se basa en tener proyectos y objetivos, sin ellos, somos</p>
+    <p>Seres Humanos vacíos </i></q> - Alejandro, agosto 2023</p><br><br>
+
+    <p><q><i>Algunas veces se gana, otras</p>
+    <p>se aprende</i></q> - Alejandro, septiembre 2019</p>
+`;
+
+marcoSobreMi.innerHTML = descripcionSobreMi;
--- js/solidaridad.js
+++ js/solidaridad.js
@@ -1,6 +1,3 @@
-var videosIDsolidaridad = ["CpCaowWDQDw"];
-var indiceSolidaridad = 0;
-
 // Función para agregar videos al carrusel
 function cargarVideos0() {
 
@@ -8,20 +5,10 @@ function cargarVideos0() {
     videosSolidaridad.innerHTML = "";
 
     var iframe = document.createElement("iframe");
-    iframe.src = "https://www.youtube.com/embed/" + videosIDsolidaridad[indiceSolidaridad];
+    iframe.src = "https://www.youtube.com/embed/CpCaowWDQDw";
     iframe.allowFullscreen = true;
     videosSolidaridad.appendChild(iframe);
 }
-
-document.getElementById("flecha-izquierda0").addEventListener("click", () => {
-    indiceSolidaridad = (indiceSolidaridad - 1 + videosIDsolidaridad.length) % videosIDsolidaridad.length;
-    cargarVideos0();
-});
-
-document.getElementById("flecha-derecha0").addEventListener("click", () => {
-    indiceSolidaridad = (indiceSolidaridad + 1) % videosIDsolidaridad.length;
-    cargarVideos0();
-});
 
 // Llamar a la función para cargar los videos cuando se muestre el modal
 $('#solidaridad').on('shown.bs.modal', function () {
