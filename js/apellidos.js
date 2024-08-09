b4a0ab6807bf3647cd5766be435ecf86dfa30cf5
soyalexokei <alejandrofernandezperez@protonmail.com>
2024-08-09 21:11:42 +0200

    104º commit

---
+++ js/apellidos.js
@@ -0,0 +1,13 @@
+const marcoMisApellidos = document.getElementById('marco-todosmisapellidos');
+
+const descripcionMisApellidos =
+`
+    <p>Mi nombre es: Alejandro</p><br>
+    <p>Mis apellidos son:</p>
+    <p>Fernández Pérez Escalante Estébanez</p>
+    <p>Megino Llanos Castaño Nieto</p>
+    <p>Nieto ? Almellones Matilla</p>
+    <p>Tineo Bayón Ruiz Alonso</p>
+`;
+
+marcoMisApellidos.innerHTML = descripcionMisApellidos;
