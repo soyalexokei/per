4dfe6b8ad34939b5506114473b48b1fb33f98550
soyalexokei <alejandrofernandezperez@protonmail.com>
2025-01-02 18:23:25 +0100

    118 commit

--- gym/js/sportTV.js
+++ gym/js/sportTV.js
@@ -2,14 +2,15 @@
 var videosIDsportTV =
 [
   "https://www.youtube.com/embed/wbfqFRH3cOQ?autoplay=0&loop=1&playlist=wbfqFRH3cOQ&controls=0&rel=0",
-  "https://www.youtube.com/embed/cWm0tmkpKlk?autoplay=0&loop=1&playlist=cWm0tmkpKlk&controls=0&rel=0"
+  "https://www.youtube.com/embed/cWm0tmkpKlk?autoplay=0&loop=1&playlist=cWm0tmkpKlk&controls=0&rel=0",
+  "https://www.youtube.com/embed/BlXqbzxGnN4?autoplay=0&loop=1&playlist=BlXqbzxGnN4&controls=0&rel=0"
 ];
   var primerasportTV = 1;
   var pantallasportTV = document.getElementById("ver-sportTV");
   var iframesportTV = document.createElement("iframe");
   
   //-- Ctes.
-  const totalsportTV = 2;
+  const totalsportTV = 3;
   
   function mostrarsportTV(auxsportTV) {
     pantallasportTV.innerHTML = "";
