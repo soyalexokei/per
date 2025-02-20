5d4f86bac7e094bc2bdbf7dfbe2ad85b7fa1d741
soyalexokei <alejandrofernandezperez@protonmail.com>
2025-02-20 17:19:07 +0100

    128 commit

---
+++ js/paises/austria.js
@@ -0,0 +1,165 @@
+//-- Variables.
+var videosIDaustria =
+[
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+    "https://www.youtube.com/embed/eyGhyAdtc30?autoplay=1&loop=1&playlist=eyGhyAdtc30&controls=0&rel=0","", "", "", 
+    "https://www.youtube.com/embed/f0PHhWc2fb8?autoplay=1&loop=1&playlist=f0PHhWc2fb8&controls=0&rel=0",
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+    "https://www.youtube.com/embed/sAN5jjplVKA?autoplay=1&loop=1&playlist=sAN5jjplVKA&controls=0&rel=0","", "", "", "", "", "", "",
+    "https://www.youtube.com/embed/QHPy_Whx1Zs?autoplay=1&loop=1&playlist=QHPy_Whx1Zs&controls=0&rel=0", "", "", "",
+    "https://www.youtube.com/embed/XAGT_qfNxlQ?autoplay=1&loop=1&playlist=XAGT_qfNxlQ&controls=0&rel=0", 
+    "", "https://www.youtube.com/embed/5LhDPCoeUFk?autoplay=1&loop=1&playlist=5LhDPCoeUFk&controls=0&rel=0",
+    "", "", "", "", "https://www.youtube.com/embed/l8x3XSBgZI8?autoplay=1&loop=1&playlist=l8x3XSBgZI8&controls=0&rel=0",
+    "", "", "", "", "", "", "", "", "", "", "", "", "", 
+    "", "", "", "", "", "", "", "", "",
+    "https://www.youtube.com/embed/7fAc5ZtVHeE?autoplay=1&loop=1&playlist=7fAc5ZtVHeE&controls=0&rel=0",
+    "", "", "", "", "", "https://www.youtube.com/embed/6_TcDwA7jHg?autoplay=1&loop=1&playlist=6_TcDwA7jHg&controls=0&rel=0", "", "", "", "", 
+    "", "", "https://www.youtube.com/embed/ZkT7xsCizCM?autoplay=1&loop=1&playlist=ZkT7xsCizCM&controls=0&rel=0", "",
+    "https://www.youtube.com/embed/WVfR0prkuDM?autoplay=1&loop=1&playlist=WVfR0prkuDM&controls=0&rel=0",
+    "", "", "", "", "", "", "https://www.youtube.com/embed/Zi8PifvkcR0?autoplay=1&loop=1&playlist=Zi8PifvkcR0&controls=0&rel=0",
+    "", "", "", "", "", "", "", "", 
+    "", "", "", "", "", "", "", "", "", "", "", "", "https://www.youtube.com/embed/mUBI75eCK9I?autoplay=1&loop=1&playlist=mUBI75eCK9I&controls=0&rel=0",
+    "", "", "", "", "", "", "",
+    "", "", "", "", "https://www.youtube.com/embed/cQetzWY6HQo?autoplay=1&loop=1&playlist=cQetzWY6HQo&controls=0&rel=0",
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 
+    "161", "162", "163", "164", "165", "166", "167", "168", "169", "170", "171", "172", "173", "174", "175", "176", "https://www.youtube.com/embed/6f7_Xea6whE?autoplay=1&loop=1&playlist=6f7_Xea6whE&controls=0&rel=0",
+    "178", "179", "180", 
+    "181", "182", "183", "184", "185", "186", "187", "188", "189", "190", "191", "192", "193", "194", "195", "196", "197", "198", "199", "200", 
+    "201", "202", "203", "204", "205", "206", "207", "208", "209", "210", "https://www.youtube.com/embed/89Hb667o4co?autoplay=1&loop=1&playlist=89Hb667o4co&controls=0&rel=0",
+    "https://www.youtube.com/embed/SO4x9dAQGac?autoplay=1&loop=1&playlist=SO4x9dAQGac&controls=0&rel=0", "213", "214", "215", "216", "217", "218", "219", "220", 
+    "221", "222", "https://www.youtube.com/embed/UAbj2_ydwho?autoplay=1&loop=1&playlist=UAbj2_ydwho&controls=0&rel=0",
+    "224", "https://www.youtube.com/embed/F9OktkDvM_Y?autoplay=1&loop=1&playlist=F9OktkDvM_Y&controls=0&rel=0",
+    "226", "https://www.youtube.com/embed/nvOjizMilOs?autoplay=1&loop=1&playlist=nvOjizMilOs&controls=0&rel=0",
+    "228", "229", "230", "231", "232", "233", "234", "235", "236", "237"
+];
+var imagenesIDaustria =
+[
+    "https://i.postimg.cc/nLxgLKNV/1.jpg","https://i.postimg.cc/0Q24Yk8L/2.jpg","https://i.postimg.cc/HnFP1bQg/3.jpg",
+    "https://i.postimg.cc/KY5V3kyP/4.jpg","https://i.postimg.cc/bN2BHFhN/5.jpg","https://i.postimg.cc/7Z3tjGhF/6.jpg",
+    "https://i.postimg.cc/fbMqsz0J/7.jpg","https://i.postimg.cc/mgf6dNL3/8.jpg","https://i.postimg.cc/nzzds32x/10.jpg",
+    "https://i.postimg.cc/nzP3qqSD/11.jpg","https://i.postimg.cc/WzqX8txf/12.jpg","https://i.postimg.cc/rz6gPMky/13.jpg",
+    "https://i.postimg.cc/HsXCsLj2/14.jpg","https://i.postimg.cc/gcwWQsW1/15.jpg","https://i.postimg.cc/tRLjj6RJ/16.jpg","",
+    "https://i.postimg.cc/13S1gZqZ/17.jpg","https://i.postimg.cc/mr2xwNjX/18.jpg","https://i.postimg.cc/NjxqmP62/19.jpg","",
+    "https://i.postimg.cc/W49BfFf2/20.jpg","https://i.postimg.cc/ydkMq1w0/21.jpg","https://i.postimg.cc/zB69qmr6/22.jpg",
+    "https://i.postimg.cc/4ycjd84d/23.jpg","https://i.postimg.cc/ZYvGVqPS/24.jpg","https://i.postimg.cc/QxpRZnnY/25.jpg",
+    "https://i.postimg.cc/jStBxQpx/26.jpg","https://i.postimg.cc/JzxvGH8b/27.jpg","https://i.postimg.cc/KYYVLccC/28.jpg",
+    "https://i.postimg.cc/0NH4Cgbj/29.jpg","https://i.postimg.cc/J0jdmGKH/30.jpg","https://i.postimg.cc/85J0Ky6H/31.jpg",
+    "https://i.postimg.cc/4dDFrzvT/32.jpg","https://i.postimg.cc/VLcVgWLq/33.jpg","https://i.postimg.cc/dQCfGxvQ/34.jpg",
+    "https://i.postimg.cc/sgj0dfW1/35.jpg","https://i.postimg.cc/TYqH45pZ/36.jpg","https://i.postimg.cc/cLFksq7h/37.jpg",
+    "https://i.postimg.cc/R0qshPXB/38.jpg","https://i.postimg.cc/4d9WbmD2/39.jpg","https://i.postimg.cc/TPSkDr4W/40.jpg",
+    "https://i.postimg.cc/90HJxcVm/41.jpg","https://i.postimg.cc/C5CJX9Lj/42.jpg","https://i.postimg.cc/c1fDvtMr/43.jpg",
+    "https://i.postimg.cc/RV9gKCXG/44.jpg","https://i.postimg.cc/hPrprFYn/45.jpg","","https://i.postimg.cc/fb3CDVRW/46.jpg",
+    "https://i.postimg.cc/P5VygWhF/47.jpg","https://i.postimg.cc/BnnN7YWK/48.jpg","https://i.postimg.cc/CKWN1wRG/49.jpg",
+    "https://i.postimg.cc/PxYMv0DB/50.jpg","https://i.postimg.cc/D0hPr5dm/51.jpg","https://i.postimg.cc/Kj5rJLJf/52.jpg","",
+    "https://i.postimg.cc/nVKkFbpm/53.jpg","https://i.postimg.cc/3JKZWSxc/54.jpg","https://i.postimg.cc/jdZ4fHWd/55.jpg","",
+    "https://i.postimg.cc/9Q0tYDTP/56.jpg","",
+    "https://i.postimg.cc/3wDXzztd/57.jpg","https://i.postimg.cc/50dBJprp/58.jpg",
+    "https://i.postimg.cc/Y961CYK7/59.jpg","https://i.postimg.cc/C5wbB5Sq/60.jpg","","https://i.postimg.cc/ry9161NC/61.jpg",
+    "https://i.postimg.cc/GtzGkRmS/62.jpg","https://i.postimg.cc/Ghhkb4M0/63.jpg","https://i.postimg.cc/BQY2bJyj/64.jpg",
+    "https://i.postimg.cc/HkrX7fDB/65.jpg","https://i.postimg.cc/zG4Ww0q5/66.jpg","https://i.postimg.cc/MGRB9VGB/67.jpg",
+    "https://i.postimg.cc/tJrnszBK/68.jpg","https://i.postimg.cc/fLN9py3r/69.jpg","https://i.postimg.cc/XJkCbWd7/70.jpg",
+    "https://i.postimg.cc/yx6S6Gyh/71.jpg","https://i.postimg.cc/gc4ZtYGg/72.jpg","https://i.postimg.cc/BQ61dd64/73.jpg",
+    "https://i.postimg.cc/fTr0ZBtX/74.jpg","https://i.postimg.cc/zGHHfKKQ/75.jpg","https://i.postimg.cc/ZqkBvnL5/76.jpg",
+    "https://i.postimg.cc/pdD9fv0Y/77.jpg","https://i.postimg.cc/mDBPVnJx/78.jpg","https://i.postimg.cc/cCJrGndX/79.jpg",
+    "https://i.postimg.cc/hjsfYrRX/80.jpg","https://i.postimg.cc/VvH5fk61/81.jpg","https://i.postimg.cc/rFKKhxnp/82.jpg","",
+    "https://i.postimg.cc/8ky7Btb3/83.jpg","https://i.postimg.cc/Kvw4CjwD/84.jpg","https://i.postimg.cc/XY5XW30S/85.jpg",
+    "https://i.postimg.cc/wBdMTP6t/86.jpg","https://i.postimg.cc/k5F4Vc4T/87.jpg","","https://i.postimg.cc/TPh5Pgvn/88.jpg",
+    "https://i.postimg.cc/5tNL11mT/89.jpg","https://i.postimg.cc/D0qQ7HR5/90.jpg","https://i.postimg.cc/gcFFGZh6/91.jpg",
+    "https://i.postimg.cc/CMxNZd9L/92.jpg","https://i.postimg.cc/dtGBvCn3/93.jpg","","https://i.postimg.cc/bNHWB8Y1/94.jpg","",
+    "https://i.postimg.cc/y8LkzJML/95.jpg","https://i.postimg.cc/3JFCX1rX/96.jpg","https://i.postimg.cc/gkSPmCQ8/97.jpg",
+    "https://i.postimg.cc/ZqFWxtb5/98.jpg","https://i.postimg.cc/s2CN60nd/99.jpg","https://i.postimg.cc/GmCMzt3n/100.jpg","",
+    "https://i.postimg.cc/VshqYY6H/101.jpg","https://i.postimg.cc/GtH6vdGm/102.jpg","https://i.postimg.cc/VLt2rTQH/103.jpg",
+    "https://i.postimg.cc/CMxcmHt3/104.jpg","https://i.postimg.cc/d3TtPgnt/105.jpg","https://i.postimg.cc/TPZhhHJD/106.jpg",
+    "https://i.postimg.cc/W4x3qcyJ/107.jpg","https://i.postimg.cc/k4bx5jHJ/108.jpg","https://i.postimg.cc/fWrWPtT4/109.jpg",
+    "https://i.postimg.cc/vm6jVj1r/110.jpg","https://i.postimg.cc/SK1Q9Dmb/111.jpg","https://i.postimg.cc/sgDtCStP/112.jpg",
+    "https://i.postimg.cc/B6mMh7bh/113.jpg","https://i.postimg.cc/NFR9Lsfj/114.jpg","https://i.postimg.cc/50Jrs9p4/115.jpg",
+    "https://i.postimg.cc/gJgq1g3r/116.jpg","https://i.postimg.cc/4yB5rRnD/117.jpg","https://i.postimg.cc/kMGSCT2D/118.jpg",
+    "https://i.postimg.cc/XYph8GRR/119.jpg","https://i.postimg.cc/KcGRkb6M/120.jpg","https://i.postimg.cc/RFXWz2d6/121.jpg","",
+    "https://i.postimg.cc/Jn1PZXjy/122.jpg","https://i.postimg.cc/5yVzjNHX/123.jpg","https://i.postimg.cc/T2sB6vXv/124.jpg",
+    "https://i.postimg.cc/8PzWRPZp/125.jpg","https://i.postimg.cc/G9LHS2Fd/126.jpg","https://i.postimg.cc/K82sq0XB/127.jpg",
+    "https://i.postimg.cc/P5WJLMk5/128.jpg","https://i.postimg.cc/3xryhnVH/129.jpg","https://i.postimg.cc/50zfs9kh/130.jpg",
+    "https://i.postimg.cc/TY9dBvjw/131.jpg","",
+    "https://i.postimg.cc/SQChBbfM/132.jpg","https://i.postimg.cc/pXMGFfZb/133.jpg",
+    "https://i.postimg.cc/k5pBPsw5/134.jpg","https://i.postimg.cc/bw07RPt3/135.jpg","https://i.postimg.cc/63n6wQ0F/136.jpg",
+    "https://i.postimg.cc/50jBC5Fc/137.jpg","https://i.postimg.cc/vHKFGpkF/138.jpg","https://i.postimg.cc/fy7j8CvV/139.jpg",
+    "https://i.postimg.cc/bwmLdwTn/140.jpg","https://i.postimg.cc/vBBtZL8N/141.jpg","https://i.postimg.cc/Kv82QZHG/142.jpg",
+    "https://i.postimg.cc/JnLZfhR5/143.jpg","https://i.postimg.cc/rsMdNw4p/144.jpg","https://i.postimg.cc/DyGvsj9S/145.jpg",
+    "https://i.postimg.cc/L4Wc2gt2/146.jpg","https://i.postimg.cc/13QTxFd5/147.jpg","https://i.postimg.cc/J4ThFsgM/148.jpg",
+    "https://i.postimg.cc/dt3bf7Jw/149.jpg","https://i.postimg.cc/GpbjSkSR/150.jpg","https://i.postimg.cc/SsX7YXZc/151.jpg",
+    "https://i.postimg.cc/jjv5NWB0/152.jpg","https://i.postimg.cc/tJDrJPkJ/153.jpg","https://i.postimg.cc/5Nzk6GMS/154.jpg",
+    "https://i.postimg.cc/x8QZgKyr/155.jpg","https://i.postimg.cc/4yCwY7jN/156.jpg","https://i.postimg.cc/DZjVgfz5/157.jpg",
+    "https://i.postimg.cc/fybyMhKm/158.jpg","https://i.postimg.cc/Pr9f7qFt/159.jpg","https://i.postimg.cc/3RYbfPBN/160.jpg",
+    "https://i.postimg.cc/VkmT3xWP/161.jpg","https://i.postimg.cc/dtZC9wn3/162.jpg","","https://i.postimg.cc/BbnGq49J/163.jpg",
+    "https://i.postimg.cc/NM90tF12/164.jpg","https://i.postimg.cc/8cgbMdLg/165.jpg","https://i.postimg.cc/NFTcfkFT/166.jpg",
+    "https://i.postimg.cc/xj3tdLzC/167.jpg","https://i.postimg.cc/NFXGhphQ/168.jpg","https://i.postimg.cc/zvGshVDy/169.jpg",
+    "https://i.postimg.cc/g2rfTdBy/170.jpg","https://i.postimg.cc/Gm1xd6DS/171.jpg","https://i.postimg.cc/wTCxBWdM/172.jpg",
+    "https://i.postimg.cc/fTtBy3nx/173.jpg","https://i.postimg.cc/W3Xspqtv/174.jpg","https://i.postimg.cc/T3RzHvnx/175.jpg",
+    "https://i.postimg.cc/9FskjxZn/176.jpg","https://i.postimg.cc/YqrkwpMt/177.jpg","https://i.postimg.cc/tCm3BRNw/178.jpg",
+    "https://i.postimg.cc/wTZmX30j/179.jpg","https://i.postimg.cc/RVCB6tG2/180.jpg","https://i.postimg.cc/HnBC4xFB/181.jpg",
+    "https://i.postimg.cc/s2YmfkwM/182.jpg","https://i.postimg.cc/4x6P6w1Y/183.jpg","https://i.postimg.cc/bY3Ddb7h/184.jpg",
+    "https://i.postimg.cc/JhNSpwHG/185.jpg","https://i.postimg.cc/KcqH88Qd/186.jpg","https://i.postimg.cc/8CMDF9GW/187.jpg",
+    "https://i.postimg.cc/W3X9bx7M/188.jpg","https://i.postimg.cc/wj0ZgPR2/189.jpg","https://i.postimg.cc/B6nXYbk2/190.jpg",
+    "https://i.postimg.cc/1tnTxLhC/191.jpg","https://i.postimg.cc/SKcpTLXw/192.jpg","https://i.postimg.cc/ZKH7tqWX/193.jpg",
+    "https://i.postimg.cc/zGRLtx9F/194.jpg","https://i.postimg.cc/4dm5kS6v/195.jpg","","","https://i.postimg.cc/7ZT4hbWm/196.jpg",
+    "https://i.postimg.cc/N09L9FL9/197.jpg","https://i.postimg.cc/Vsb3ChR7/198.jpg","https://i.postimg.cc/5tTqQ5fT/199.jpg",
+    "https://i.postimg.cc/BQqfKkHC/200.jpg","https://i.postimg.cc/QdpdxMS3/201.jpg","https://i.postimg.cc/8cm9K5Sj/202.jpg",
+    "https://i.postimg.cc/Xq6ZrKBP/203.jpg","https://i.postimg.cc/2StDFmfG/204.jpg","https://i.postimg.cc/m2ys7yqH/205.jpg","",
+    "https://i.postimg.cc/d16cXtQv/206.jpg","",
+    "https://i.postimg.cc/MKsBQ26k/207.jpg","",
+    "https://i.postimg.cc/VkZzq1CD/208.jpg",
+    "https://i.postimg.cc/Z5Q2T1My/209.jpg","https://i.postimg.cc/SsBNXkPp/210.jpg","https://i.postimg.cc/Y9hLb1MQ/211.jpg",
+    "https://i.postimg.cc/CMtd2Cb6/212.jpg","https://i.postimg.cc/Vk2tKqY5/213.jpg","https://i.postimg.cc/YCHDpF7m/214.jpg",
+    "https://i.postimg.cc/Kj5PRQSn/215.jpg","https://i.postimg.cc/qvDqXBtt/216.jpg","https://i.postimg.cc/3xZYy0Mv/217.jpg"
+];
+var indiceAustria = 1;
+var pantallaAustria = document.getElementById('ver-austria');
+var imgAustria = document.createElement("img");
+var iframeAustria = document.createElement("iframe");
+
+//-- Ctes.
+const totalAustria = 237;
+
+function mostrarAustria(auxAustria) {
+    
+    //-- Limpiar la pantalla.
+    pantallaAustria.innerHTML = "";
+
+    //-- Verificar si es una imagen o un vídeo.
+    if(imagenesIDaustria[auxAustria-1]) {
+        imgAustria.src = imagenesIDaustria[auxAustria-1];
+        pantallaAustria.appendChild(imgAustria);
+    }else {
+        iframeAustria.src =  videosIDaustria[auxAustria-1];
+        iframeAustria.width = "400";
+        iframeAustria.height = "533";
+        iframeAustria.setAttribute("allow", "autoplay");
+        iframeAustria.setAttribute("allowFullscreen", "false");
+        pantallaAustria.appendChild(iframeAustria);
+    }
+}
+
+function imgAnteriorAustria() {
+    if(indiceAustria > 1) {
+        indiceAustria--;
+    }else {
+        indiceAustria = totalAustria;
+    }
+    mostrarAustria(indiceAustria);
+}
+
+function imgSiguienteAustria() {
+    if(indiceAustria < totalAustria) {
+        indiceAustria++;
+    }else {
+        indiceAustria = 1;
+    }
+    mostrarAustria(indiceAustria);
+}
+
+//-- Pulsar flecha izquierda.
+document.getElementById("izq-austria").addEventListener("click", imgAnteriorAustria);
+//-- Pulsar flecha derecha.
+document.getElementById("der-austria").addEventListener("click", imgSiguienteAustria);
+
+//-- Punto de inicio del programa.
+mostrarAustria(indiceAustria);
