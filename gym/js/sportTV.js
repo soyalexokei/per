a686ab4998e0c6220bfa9b2e6c88bc9722a4f78f
soyalexokei <alejandrofernandezperez@protonmail.com>
2025-02-28 18:39:53 +0100

    133 commit

--- gym/js/sportTV.js
+++ gym/js/sportTV.js
@@ -4,14 +4,15 @@ var videosIDsportTV =
   "https://www.youtube.com/embed/wbfqFRH3cOQ?autoplay=0&loop=1&playlist=wbfqFRH3cOQ&controls=0&rel=0",
   "https://www.youtube.com/embed/cWm0tmkpKlk?autoplay=0&loop=1&playlist=cWm0tmkpKlk&controls=0&rel=0",
   "https://www.youtube.com/embed/BlXqbzxGnN4?autoplay=0&loop=1&playlist=BlXqbzxGnN4&controls=0&rel=0",
-  "https://www.youtube.com/embed/FmLeT9L7-kI?autoplay=0&loop=1&playlist=FmLeT9L7-kI&controls=0&rel=0"
+  "https://www.youtube.com/embed/FmLeT9L7-kI?autoplay=0&loop=1&playlist=FmLeT9L7-kI&controls=0&rel=0",
+  "https://www.youtube.com/embed/7wK_Xh8mc0A?autoplay=0&loop=1&playlist=7wK_Xh8mc0A&controls=0&rel=0"
 ];
   var primerasportTV = 1;
   var pantallasportTV = document.getElementById("ver-sportTV");
   var iframesportTV = document.createElement("iframe");
   
   //-- Ctes.
-  const totalsportTV = 4;
+  const totalsportTV = 5;
   
   function mostrarsportTV(auxsportTV) {
     pantallasportTV.innerHTML = "";
--- js/paises/uk.js
+++ js/paises/uk.js
@@ -1,7 +1,7 @@
 //-- Variables.
 var videosIDuk =
 [
-    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
     "", "", "https://www.youtube.com/embed/tedzX0xufsM?autoplay=1&loop=1&playlist=tedzX0xufsM&controls=0&rel=0",
     "https://www.youtube.com/embed/s3GpWv21uVM?autoplay=1&loop=1&playlist=s3GpWv21uVM&controls=0&rel=0",
     "","https://www.youtube.com/embed/GiRyIoHuMxY?autoplay=1&loop=1&playlist=GiRyIoHuMxY&controls=0&rel=0",
@@ -19,11 +19,13 @@ var imagenesIDuk =
     "https://i.postimg.cc/fT68SfgS/07.jpg", "https://i.postimg.cc/vH1PmNXd/08.jpg", "https://i.postimg.cc/fbs5BTsw/09.jpg",
     "https://i.postimg.cc/FRspXW18/10.jpg", "https://i.postimg.cc/85fznDNC/11.jpg", "https://i.postimg.cc/bJCTyxtJ/12.jpg",
     "https://i.postimg.cc/gc7D1Kpt/13.jpg", "https://i.postimg.cc/rFtJTtFV/14.jpg", "https://i.postimg.cc/qvsxhV3W/15.jpg",
-    "https://i.postimg.cc/Dwkcwy8q/16.jpg", "https://i.postimg.cc/HLT4bZ0n/17.jpg", "https://i.postimg.cc/43wvP6w5/18.jpg",
+    "https://i.postimg.cc/Dwkcwy8q/16.jpg", "https://i.postimg.cc/Dwgk1Jmh/33.png", "https://i.postimg.cc/t4yG1tqD/34.png",
+    "https://i.postimg.cc/28nf8XDV/35.png", "https://i.postimg.cc/HLT4bZ0n/17.jpg", "https://i.postimg.cc/43wvP6w5/18.jpg",
     "https://i.postimg.cc/76W38qFJ/19.jpg", "https://i.postimg.cc/MHfyzhwP/20.jpg", "https://i.postimg.cc/bYZMGrNM/21.jpg",
     "https://i.postimg.cc/05h47vzN/22.jpg", "https://i.postimg.cc/K82ss5rS/23.jpg", "https://i.postimg.cc/nhGw8Q0s/24.jpg",
-    "https://i.postimg.cc/FKPWzXtN/25.jpg", "https://i.postimg.cc/B6Xh17sD/26.jpg", "https://i.postimg.cc/ydtLhhYG/27.jpg",
-    "https://i.postimg.cc/7hzmKC5Z/28.jpg", "https://i.postimg.cc/qB6bJF1H/29.jpg", "https://i.postimg.cc/3JMLdZq4/30.jpg",
+    "https://i.postimg.cc/FKPWzXtN/25.jpg", "https://i.postimg.cc/P5ZNHM64/38.png", "https://i.postimg.cc/B6Xh17sD/26.jpg", "https://i.postimg.cc/ydtLhhYG/27.jpg",
+    "https://i.postimg.cc/7hzmKC5Z/28.jpg", "https://i.postimg.cc/QN1XHMg8/36.png", "https://i.postimg.cc/15Sy6ZMp/37.png",
+    "https://i.postimg.cc/qB6bJF1H/29.jpg", "https://i.postimg.cc/3JMLdZq4/30.jpg",
     "https://i.postimg.cc/dt85dyMW/31.jpg", "https://i.postimg.cc/mgB8SgBT/32.jpg", "https://i.postimg.cc/dtHkCkk8/1.png",
     "https://i.postimg.cc/PrrvNzxX/2.png", "","","https://i.postimg.cc/9QsDx6B0/5.png", "","","","https://i.postimg.cc/138grmwb/9.png",
     "https://i.postimg.cc/d3Q3kwJH/10.png", "https://i.postimg.cc/pTJyLpM5/11.png", "https://i.postimg.cc/k4mBCWzd/12.png",
@@ -38,7 +40,7 @@ var imgUK = document.createElement("img");
 var iframeUK = document.createElement("iframe");
 
 //-- Ctes.
-const totalUK = 57;
+const totalUK = 63;
 
 function mostrarUK(auxUK) {
     
