11cccbc63e57e42ba3f0e3e90735106af52060d1
soyalexokei <alejandrofernandezperez@protonmail.com>
2025-03-03 00:56:31 +0100

    134 commit

---
+++ js/paises/kosovo.js
@@ -0,0 +1,189 @@
+//-- Variables.
+var videosIDkosovo =
+[
+    "",
+    "https://www.youtube.com/embed/Vug9yUIbGGw?autoplay=1&loop=1&playlist=Vug9yUIbGGw&controls=0&rel=0",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "https://www.youtube.com/embed/prEXA0HiHdc?autoplay=1&loop=1&playlist=prEXA0HiHdc&controls=0&rel=0",
+    "",
+    "https://www.youtube.com/embed/NOlUcPd2Ens?autoplay=1&loop=1&playlist=NOlUcPd2Ens&controls=0&rel=0",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "https://www.youtube.com/embed/qgC98tuh_fo?autoplay=1&loop=1&playlist=qgC98tuh_fo&controls=0&rel=0",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "https://www.youtube.com/embed/_dlDVlVk_NA?autoplay=1&loop=1&playlist=_dlDVlVk_NA&controls=0&rel=0",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "https://www.youtube.com/embed/qPvKur-jnzI?autoplay=1&loop=1&playlist=qPvKur-jnzI&controls=0&rel=0",
+    "https://www.youtube.com/embed/aGEblhSjLkA?autoplay=1&loop=1&playlist=aGEblhSjLkA&controls=0&rel=0",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "https://www.youtube.com/embed/lrq6L_GXr3U?autoplay=1&loop=1&playlist=lrq6L_GXr3U&controls=0&rel=0",
+    "https://www.youtube.com/embed/KDSLhfilP_s?autoplay=1&loop=1&playlist=KDSLhfilP_s&controls=0&rel=0",
+    "",
+    "",
+    "",
+    "",
+    "",
+];
+var imagenesIDkosovo =
+[
+    "https://i.postimg.cc/PrhbRKN8/1.jpg",
+    "",
+    "https://i.postimg.cc/HL2jj4dF/2.jpg",
+    "https://i.postimg.cc/ncyXWh3S/3.jpg",
+    "https://i.postimg.cc/Dw1S33Dv/4.jpg",
+    "https://i.postimg.cc/pXh9kFJn/5.jpg",
+    "https://i.postimg.cc/FKm79n5j/6.jpg",
+    "",
+    "https://i.postimg.cc/htGKpWBZ/7.jpg",
+    "",
+    "https://i.postimg.cc/GhXxZYW3/IMG-20220416-WA0007.png",
+    "https://i.postimg.cc/Hkj9Hnmw/IMG-20220416-WA0005.png",
+    "https://i.postimg.cc/ydTZ0CB6/8.jpg",
+    "https://i.postimg.cc/W3VZDbBf/9.jpg",
+    "https://i.postimg.cc/m21rsWqc/10.jpg",
+    "https://i.postimg.cc/C5zx53xQ/11.jpg",
+    "https://i.postimg.cc/QCwxTfBx/12.jpg",
+    "https://i.postimg.cc/wxGBZjW8/13.jpg",
+    "",
+    "https://i.postimg.cc/XYM9M82b/14.jpg",
+    "https://i.postimg.cc/kMpxs9KD/15.jpg",
+    "https://i.postimg.cc/Z5rpJdgy/16.jpg",
+    "https://i.postimg.cc/kXRWhZvs/17.jpg",
+    "https://i.postimg.cc/bwz1gR3N/IMG-20220416-134607.png",
+    "https://i.postimg.cc/hGQv1zWt/18.jpg",
+    "https://i.postimg.cc/nz8zRFpY/19.jpg",
+    "https://i.postimg.cc/RZDqt1YY/20.jpg",
+    "https://i.postimg.cc/Y91ST5Sc/21.jpg",
+    "https://i.postimg.cc/xTnxzLDn/IMG-20220416-133405.png",
+    "https://i.postimg.cc/RFwd3ZgW/IMG-20220416-WA0010.png",
+    "https://i.postimg.cc/d3B9BSPv/IMG-20220416-163157.png",
+    "https://i.postimg.cc/GtrQvnfq/IMG-20220416-141757.png",
+    "",
+    "https://i.postimg.cc/ZK5PmHzB/26.jpg",
+    "https://i.postimg.cc/76N78GBn/22.jpg",
+    "https://i.postimg.cc/kG1bDrPV/23.jpg",
+    "https://i.postimg.cc/8CRfMfVg/IMG-20220416-135929.png",
+    "https://i.postimg.cc/RhVtMSy5/24.jpg",
+    "https://i.postimg.cc/7Pm7KzJx/25.jpg",
+    "https://i.postimg.cc/DzkbSdHc/IMG-20220416-134312.png",
+    "https://i.postimg.cc/15XF2zDD/27.jpg",
+    "",
+    "",
+    "https://i.postimg.cc/NMVt0bPs/28.jpg",
+    "https://i.postimg.cc/G29rr6Kg/29.jpg",
+    "https://i.postimg.cc/28SttnHT/IMG-20220417-000013.png",
+    "https://i.postimg.cc/65bPxTgK/IMG-20220417-001646.png",
+    "https://i.postimg.cc/PxfbqQvR/32.jpg",
+    "https://i.postimg.cc/cC2fXq9p/30.jpg",
+    "https://i.postimg.cc/L8Hbrwmc/IMG-20220417-093025.png",
+    "https://i.postimg.cc/ncKWSpfK/IMG-20220417-093026.png",
+    "https://i.postimg.cc/fbygbdt5/IMG-20220417-093049.png",
+    "https://i.postimg.cc/6QPb4QCK/IMG-20220417-093122.png",
+    "https://i.postimg.cc/CxLcv8NY/IMG-20220417-093129.png",
+    "https://i.postimg.cc/4N18bPpB/IMG-20220417-093138.png",
+    "https://i.postimg.cc/DyhjXH4S/IMG-20220417-093258.png",
+    "https://i.postimg.cc/MpPdgGPB/IMG-20220417-093318.png",
+    "https://i.postimg.cc/ZqXjKMhK/Sin-t-tulo-1.png",
+    "",
+    "",
+    "https://i.postimg.cc/nVSrjbPk/31.jpg",
+    "https://i.postimg.cc/qqq7bjzy/33.jpg",
+    "https://i.postimg.cc/9MJMYvFc/34.jpg",
+    "https://i.postimg.cc/sxLgt7h8/35.jpg",
+    "https://i.postimg.cc/fT3LYq4h/36.jpg",
+];
+var indiceKosovo = 1;
+var pantallaKosovo = document.getElementById('ver-kosovo');
+var imgKosovo = document.createElement("img");
+var iframeKosovo = document.createElement("iframe");
+
+//-- Ctes.
+const totalKosovo = 65;
+
+function mostrarKosovo(auxKosovo) {
+    
+    //-- Limpiar la pantalla.
+    pantallaKosovo.innerHTML = "";
+
+    //-- Verificar si es una imagen o un vídeo.
+    if(imagenesIDkosovo[auxKosovo-1]) {
+        imgKosovo.src = imagenesIDkosovo[auxKosovo-1];
+        pantallaKosovo.appendChild(imgKosovo);
+    }else {
+        iframeKosovo.src = videosIDkosovo[auxKosovo-1];
+        iframeKosovo.width = "400";
+        iframeKosovo.height = "533";
+        iframeKosovo.setAttribute("allow", "autoplay");
+        iframeKosovo.setAttribute("allowFullscreen", "false");
+        pantallaKosovo.appendChild(iframeKosovo);
+    }
+}
+
+function imgAnteriorKosovo() {
+    if(indiceKosovo > 1) {
+        indiceKosovo--;
+    }else {
+        indiceKosovo = totalKosovo;
+    }
+    mostrarKosovo(indiceKosovo);
+}
+
+function imgSiguienteKosovo() {
+    if(indiceKosovo < totalKosovo) {
+        indiceKosovo++;
+    }else {
+        indiceKosovo = 1;
+    }
+    mostrarKosovo(indiceKosovo);
+}
+
+//-- Pulsar flecha izquierda.
+document.getElementById("izq-kosovo").addEventListener("click", imgAnteriorKosovo);
+//-- Pulsar flecha derecha.
+document.getElementById("der-kosovo").addEventListener("click", imgSiguienteKosovo);
+
+//-- Punto de inicio del programa.
+mostrarKosovo(indiceKosovo);
