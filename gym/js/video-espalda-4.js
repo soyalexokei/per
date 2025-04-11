30e3e313e333ceddbd4416186b85b2fe975495a6
soyalexokei <alejandrofernandezperez@protonmail.com>
2025-04-11 12:27:33 +0200

    169 commit

--- gym/js/video-espalda-4.js
+++ gym/js/video-espalda-4.js
@@ -5,7 +5,7 @@ function cargarEspalda4() {
     videosEspalda4.innerHTML = "";
 
     var iframe = document.createElement("iframe");
-    iframe.src = "https://www.youtube.com/embed/ue8MXKXdOVw";
+    iframe.src = "https://www.youtube.com/embed/OoG6XtJeygk";
     iframe.allowFullscreen = true;
     videosEspalda4.appendChild(iframe);
 }
