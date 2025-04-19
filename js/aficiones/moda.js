bfb99e4c055fc89347b41bb7f53fbf9eee396a9f
soyalexokei <alejandrofernandezperez@protonmail.com>
2025-04-19 17:15:59 +0200

    172 commit

--- js/aficiones/moda.js
+++ js/aficiones/moda.js
@@ -5,14 +5,15 @@ var imagenesIDfashionMen =
   "https://i.postimg.cc/6qJg0Xg7/004.png", "https://i.postimg.cc/9MqsZM85/005.png", "https://i.postimg.cc/R0YjWfTL/006.png",
   "https://i.postimg.cc/7h3ctRrK/007.png", "https://i.postimg.cc/13vxZbnT/008.png", "https://i.postimg.cc/6Qqk6zCQ/009.png",
   "https://i.postimg.cc/ZKFtwWTJ/010.png", "https://i.postimg.cc/T3d8FTwg/011.png", "https://i.postimg.cc/QxrZgQZ6/012.png",
-  "https://i.postimg.cc/8kvVry9J/013.png", "https://i.postimg.cc/1XDhhtwc/014.png", "https://i.postimg.cc/xCwD4HJ1/015.png"
+  "https://i.postimg.cc/8kvVry9J/013.png", "https://i.postimg.cc/rsDCKZZX/IMG-20250419-131150.png",
+  "https://i.postimg.cc/1XDhhtwc/014.png", "https://i.postimg.cc/xCwD4HJ1/015.png"
 ];
   var primerafashionMen = 1;
   var pantallafashionMen = document.getElementById("ver-fashionMen");
   var imgfashionMen = document.createElement("img");
   
   //-- Ctes.
-  const totalfashionMen = 15;
+  const totalfashionMen = 16;
   
   function mostrarfashionMen(auxfashionMen) {
     
