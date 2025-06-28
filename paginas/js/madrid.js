f93a294cc70e9b4ef09961dcab3703a825e73ab6
soyalexokei <alejandrofernandezperez@protonmail.com>
2025-06-28 18:32:27 +0200

    195 commit

--- paginas/js/madrid.js
+++ paginas/js/madrid.js
@@ -1,61 +1,70 @@
 //-- Variables. https://www.youtube.com/embed/ ?autoplay=1&loop=1&playlist= &controls=0&rel=0
-var videosID =
+var videosIDmadrid =
 [
-    
+    "",
+    "",
+    "",
+    "",
+    "",
 ];
-var imagenesID =
+var imagenesIDmadrid =
 [
-    
+    /* Junio de 2025 - Despedida de Soltero de Colombi */
+    "https://i.postimg.cc/x8dkCWf7/IMG-20250627-154547.png",
+    "https://i.postimg.cc/t70rytys/IMG-20250627-154044.png",
+    "https://i.postimg.cc/63Nt53xq/IMG-20250628-181859.png",
+    "https://i.postimg.cc/nzYZsJnk/IMG-20250628-181916.png",
+    "https://i.postimg.cc/BvFJ0FyR/IMG-20250628-182752-784.webp",
 ];
-var indice = 1;
-var pantalla = document.getElementById('ver-');
-var img = document.createElement("img");
-var iframe = document.createElement("iframe");
+var indicemadrid = 1;
+var pantallamadrid = document.getElementById('ver-madrid');
+var imgmadrid = document.createElement("img");
+var iframemadrid = document.createElement("iframe");
 
 //-- Ctes.
-const total = 0;
+const totalmadrid = 5;
 
-function mostrar(aux) {
+function mostrarmadrid(auxmadrid) {
     
     //-- Limpiar la pantalla.
-    pantalla.innerHTML = "";
+    pantallamadrid.innerHTML = "";
 
     //-- Verificar si es una imagen o un vídeo.
-    if(imagenesID[aux-1]) {
-        img.src = imagenesID[aux-1];
-        pantalla.appendChild(img);
+    if(imagenesIDmadrid[auxmadrid-1]) {
+        imgmadrid.src = imagenesIDmadrid[auxmadrid-1];
+        pantallamadrid.appendChild(imgmadrid);
     }else {
-        iframe.src = videosID[aux-1];
-        iframe.width = "400";
-        iframe.height = "533";
-        iframe.setAttribute("allow", "autoplay");
-        iframe.setAttribute("allowFullscreen", "false");
-        pantalla.appendChild(iframe);
+        iframemadrid.src = videosIDmadrid[auxmadrid-1];
+        iframemadrid.width = "400";
+        iframemadrid.height = "533";
+        iframemadrid.setAttribute("allow", "autoplay");
+        iframemadrid.setAttribute("allowFullscreen", "false");
+        pantallamadrid.appendChild(iframemadrid);
     }
 }
 
-function imgAnterior() {
-    if(indice > 1) {
-        indice--;
+function imgAnteriormadrid() {
+    if(indicemadrid > 1) {
+        indicemadrid--;
     }else {
-        indice = total;
+        indicemadrid = totalmadrid;
     }
-    mostrar(indice);
+    mostrarmadrid(indicemadrid);
 }
 
-function imgSiguiente() {
-    if(indice < total) {
-        indice++;
+function imgSiguientemadrid() {
+    if(indicemadrid < totalmadrid) {
+        indicemadrid++;
     }else {
-        indice = 1;
+        indicemadrid = 1;
     }
-    mostrar(indice);
+    mostrarmadrid(indicemadrid);
 }
 
 //-- Pulsar flecha izquierda.
-document.getElementById("izq-").addEventListener("click", imgAnterior);
+document.getElementById("izq-madrid").addEventListener("click", imgAnteriormadrid);
 //-- Pulsar flecha derecha.
-document.getElementById("der-").addEventListener("click", imgSiguiente);
+document.getElementById("der-madrid").addEventListener("click", imgSiguientemadrid);
 
 //-- Punto de inicio del programa.
-mostrar(indice);
+mostrarmadrid(indicemadrid);
