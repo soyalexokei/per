8ac8fb6fe2e7b93a5563c5b3b794aa005acfef9f
soyalexokei <alejandrofernandezperez@protonmail.com>
2025-10-18 19:18:10 +0200

    Edicion Per Web

--- media/paises/italy/italy.js
+++ media/paises/italy/italy.js
@@ -4,7 +4,33 @@
 */
 const media =
 [
-    
+  { type: "image", src: "https://i.postimg.cc/cHxQ4rKw/IMG-20251018-161332-944.webp" },
+  { type: "image", src: "https://i.postimg.cc/tTqhCY1h/IMG-20251018-164751-471.webp" },
+  { type: "image", src: "https://i.postimg.cc/vBYWHc1c/IMG-20251018-161818-580.webp" },
+  { type: "image", src: "https://i.postimg.cc/xjHzDvpv/IMG-20251018-164829-075.webp" },
+  { type: "image", src: "https://i.postimg.cc/1XpnGSNh/IMG-20251018-164955-279.webp" },
+  { type: "image", src: "https://i.postimg.cc/Vvjrqw0F/IMG-20251018-165838.png" },
+  { type: "image", src: "https://i.postimg.cc/4yz76GHQ/IMG-20251018-165852.png" },
+  { type: "image", src: "https://i.postimg.cc/MHkffHHP/IMG-20251018-170838-860.webp" },
+  { type: "image", src: "https://i.postimg.cc/L5zYtSgT/IMG-20251018-171122.png" },
+  { type: "youtube", src: "https://www.youtube.com/embed/b0ydA_6OgSI" },
+  { type: "image", src: "https://i.postimg.cc/fbGYmBgD/IMG-20251018-171916.png" },
+  { type: "image", src: "https://i.postimg.cc/nrHjG5K7/IMG-20251018-174454.png" },
+  { type: "youtube", src: "https://www.youtube.com/embed/Ye1qfVmkRiM" },
+  { type: "image", src: "https://i.postimg.cc/4dKsk1JL/IMG-20251018-172620.png" },
+  { type: "youtube", src: "https://www.youtube.com/embed/hdLt4hzpziM" },
+  { type: "image", src: "https://i.postimg.cc/SsLVKBN4/IMG-20251018-171335.png" },
+  { type: "image", src: "https://i.postimg.cc/nzKTLycJ/IMG-20251018-171655.png" },
+  { type: "image", src: "https://i.postimg.cc/DZ3Bnvhp/IMG-20251018-174349.png" },
+  { type: "image", src: "https://i.postimg.cc/QtZ0h8DQ/IMG-20251018-174416.png" },
+  { type: "image", src: "https://i.postimg.cc/wBm0CtC3/IMG-20251018-174728-584.webp" },
+  { type: "image", src: "https://i.postimg.cc/g0ZNCwCc/IMG-20251018-175916.png" },
+  { type: "image", src: "https://i.postimg.cc/g0ZNCwCJ/IMG-20251018-175837.png" },
+  { type: "image", src: "https://i.postimg.cc/zfg0sLsz/IMG-20251018-175857.png" },
+  { type: "image", src: "https://i.postimg.cc/GpyqWBWz/IMG-20251018-174439.png" },
+  { type: "image", src: "https://i.postimg.cc/Mp4930FT/IMG-20251018-180055-063.webp" },
+  { type: "image", src: "https://i.postimg.cc/DZQ5z9ft/IMG-20251018-173902-439.webp" },
+  { type: "image", src: "https://i.postimg.cc/vBw3bYyX/IMG-20251018-174403.png" },
 ];
 
 let currentIndex = 1;
