beb04eabda6c49094a9e5a7a8ed28d54d80472cf
soyalexokei <alejandrofernandezperez@protonmail.com>
2026-05-29 01:27:13 +0200

    New version for the Personal Web

--- js/aficiones/aimusic.js
+++ js/aficiones/aimusic.js
@@ -1,21 +1,18 @@
 //-- Variables.
 var videosIDaimusic =
 [
-    "https://www.youtube.com/embed/8X7JNaKj4Gk?autoplay=0&loop=1&playlist=8X7JNaKj4Gk&controls=0&rel=0",
-    "https://www.youtube.com/embed/WL9rZr_iqcU?autoplay=0&loop=1&playlist=WL9rZr_iqcU&controls=0&rel=0",
-    "https://www.youtube.com/embed/HN6CdXciI_8?autoplay=0&loop=1&playlist=HN6CdXciI_8&controls=0&rel=0",
-    "https://www.youtube.com/embed/cAbLfUpnI5U?autoplay=0&loop=1&playlist=cAbLfUpnI5U&controls=0&rel=0",
-    "https://www.youtube.com/embed/_N7QaKqi1a0?autoplay=0&loop=1&playlist=_N7QaKqi1a0&controls=0&rel=0",
-    "https://www.youtube.com/embed/Rou9l8VA2F4?autoplay=0&loop=1&playlist=Rou9l8VA2F4&controls=0&rel=0",
-    "https://www.youtube.com/embed/_FSB4nzU23M?autoplay=0&loop=1&playlist=_FSB4nzU23M&controls=0&rel=0",
-    "https://www.youtube.com/embed/K6DOmuJkXuk?autoplay=0&loop=1&playlist=K6DOmuJkXuk&controls=0&rel=0",
+  "https://www.youtube.com/embed/8X7JNaKj4Gk?autoplay=0&loop=1&playlist=8X7JNaKj4Gk&controls=0&rel=0",
+  "https://www.youtube.com/embed/WL9rZr_iqcU?autoplay=0&loop=1&playlist=WL9rZr_iqcU&controls=0&rel=0",
+  "https://www.youtube.com/embed/HN6CdXciI_8?autoplay=0&loop=1&playlist=HN6CdXciI_8&controls=0&rel=0",
+  "https://www.youtube.com/embed/cAbLfUpnI5U?autoplay=0&loop=1&playlist=cAbLfUpnI5U&controls=0&rel=0",
+  "https://www.youtube.com/embed/_N7QaKqi1a0?autoplay=0&loop=1&playlist=_N7QaKqi1a0&controls=0&rel=0",
+  "https://www.youtube.com/embed/Rou9l8VA2F4?autoplay=0&loop=1&playlist=Rou9l8VA2F4&controls=0&rel=0",
+  "https://www.youtube.com/embed/_FSB4nzU23M?autoplay=0&loop=1&playlist=_FSB4nzU23M&controls=0&rel=0",
 ];
   var primeraaimusic = 1;
   var pantallaaimusic = document.getElementById("ver-aimusic");
   var iframeaimusic = document.createElement("iframe");
-  
-  //-- Ctes.
-  const totalaimusic = 8;
+  var totalaimusic = videosIDaimusic.length;
   
   function mostraraimusic(auxaimusic) {
     
